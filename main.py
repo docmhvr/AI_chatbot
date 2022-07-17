324fbc8e91f43a03d8cc1ebc96d6734e6b722537
mahavir <mahavir37chandaliya@gmail.com>
2022-07-16 17:39:28 -0700

    getting data ready for model

--- main.py
+++ main.py
@@ -29,3 +29,36 @@ for intent in data['intents']:
         
     if intent['tag'] not in labels:
         labels.append(intent['tag'])
+
+# stemming & words and labels sorted and unique
+words = [stemmer.stem(w.lower()) for w in words if w != "?"] # to get to roots 
+words = sorted(list(set(words))) # removing duplicates
+
+labels = sorted(labels)
+
+# lists for number of words and labels for model
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+# converting data to feed to model
+for x, doc in enumerate(docs_x):
+    bag = []
+    wrds = [stemmer.stem(w.lower()) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag) # [1,2,0,5,4,2,1,...] no. of root words
+    output.append(output_row) # [] similar for labels
+
+# converting lists to numpy arrays
+training = numpy.array(training)
+output = numpy.array(output)

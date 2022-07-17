fe6decbb92e0176e290a13da0599e62ee3024eb7
mahavir <mahavir37chandaliya@gmail.com>
2022-07-16 17:11:17 -0700

    Loaded words and tags from json to py lists

--- main.py
+++ main.py
@@ -6,4 +6,26 @@ import numpy
 import tflearn
 import tensorflow
 import random
+import json
 
+with open('intents.json') as file:
+    data = json.load(file)
+
+# print(data["intents"])
+
+# Extracting data to lists
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+# Stemming .... loading tags and words data
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
+        
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
